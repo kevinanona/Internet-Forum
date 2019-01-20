31cef72cdebb322566f2a6de31202bc8de1a8499
Kevin Melo <k_elloco@hotmail.com>
2019-01-20 15:00:30 -0500

    Add files via upload

---
+++ verification.php
@@ -0,0 +1,40 @@
+<?php
+echo "<!DOCTYPE HTML>";
+echo "<HTML><BODY>";
+
+require_once "../database.php";
+//echo "*A* <br>";
+//if(isset($_GET['userid']) && isset($_GET['validate'])){
+	//echo "*B <br>*";
+	$username = intval($_GET['username']);
+	echo "The Username is $username";
+	//echo $userid;
+	//echo "<br>";
+	//$validate = mysql_fix_string ($dbcon, ($_GET['validate']));
+	//echo "$validate <br>";
+	$verified = 0;
+	$result = mysqli_query($dbcon, "SELECT * FROM Users Where username = '$username' AND verified = '$verified'");
+	$num_rows = mysqli_num_rows($result);
+	
+	if(!num_rows){
+		echo $result->num_rows;
+		die("User doesnt exist");
+	}
+	else{
+		echo "succesfully verified, go to http://weblab.salemstate.edu/~csforum/Forum/login.html to log in";
+		$update = dbquery("UPDATE Users SET verified = '1' WHERE username = '$username'");
+	}
+	
+//}
+
+function mysql_fix_string($dbcon, $string)
+{
+	if (get_magic_quotes_gpc()) $string = stripslashes($string);
+	return $dbcon->real_escape_string($string);
+}
+
+
+echo "</BODY></HTML>";
+
+
+?>
