e3e093cdec74c77f097c38d716988f0c24e15074
Kevin Melo <k_elloco@hotmail.com>
2019-02-07 02:14:31 -0500

    added use of mysql_fix_string function on a sql attribute

--- verification.php
+++ verification.php
@@ -6,13 +6,14 @@ require_once "../database.php";
 //echo "*A* <br>";
 //if(isset($_GET['userid']) && isset($_GET['validate'])){
 	//echo "*B <br>*";
-	$username = intval($_GET['username']);
-	echo "The Username is $username";
+	$username = ($_GET['username']);
+	$verified = mysql_fix_string ($dbcon, ($_GET['verified']));
+	echo "The Username is $username<br>";
 	//echo $userid;
 	//echo "<br>";
 	//$validate = mysql_fix_string ($dbcon, ($_GET['validate']));
 	//echo "$validate <br>";
-	$verified = 0;
+	//$verified = 0;
 	$result = mysqli_query($dbcon, "SELECT * FROM Users Where username = '$username' AND verified = '$verified'");
 	$num_rows = mysqli_num_rows($result);
 	
