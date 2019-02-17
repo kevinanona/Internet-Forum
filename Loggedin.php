8c83a400c7c00bd44846fd50bd0738353d5d29d4
Kevin Melo <k_elloco@hotmail.com>
2019-02-17 01:24:35 -0500

    Cleaned the file up by getting rid of the <<<END statement and just writing the HTML portion outside of the php field. Also made the file point to a new php file, displayForums.php which is in charge of getting the Forum rows from the database and displaying it in the webpage

--- Loggedin.php
+++ Loggedin.php
@@ -39,15 +39,55 @@ window.onclick = function(event) {
 	</FORM>
 	</DIV>
 	
-	<DIV id=searchField><button onmouseover=subjectList()>Tags</button> <input name="searchBar" placeholder="Enter forum tag"></DIV>
+	<div id="searching">
+  <select id="searchOptions"
+          onchange="document.getElementById('displayValue').value=this.options[this.selectedIndex].text; document.getElementById('idValue').value=this.options[this.selectedIndex].value;">
+	<option value="CSC 105">CSC 105</option>
+    <option value="CSC 110">CSC 110</option>
+    <option value="CSC 115">CSC 115</option>
+	<option value="CSC 212">CSC 212</option>
+	<option value="CSC 235">CSC 235</option>
+	<option value="CSC 246">CSC 246</option>
+	<option value="CSC 260">CSC 260</option>
+	<option value="CSC 263">CSC 263</option>
+	<option value="CSC 278">CSC 278</option>
+	<option value="CSC 279">CSC 279</option>
+	<option value="CSC 295">CSC 295</option>
+	<option value="CSC 300">CSC 300</option>
+	<option value="CSC 315A">CSC 315A</option>
+	<option value="CSC 325">CSC 325</option>
+	<option value="CSC 340">CSC 340</option>
+	<option value="CSC 345">CSC 345</option>
+	<option value="CSC 351">CSC 351</option>
+	<option value="CSC 367">CSC 367</option>
+	<option value="CSC 376">CSC 376</option>
+	<option value="CSC 381">CSC 381</option>
+	<option value="CSC 400">CSC 400</option>
+	<option value="CSC 415">CSC 415</option>
+	<option value="CSC 425">CSC 425</option>
+	<option value="CSC 435">CSC 435</option>
+	<option value="CSC 445">CSC 445</option>
+	<option value="CSC 475">CSC 475</option>
+	<option value="CSC 485">CSC 485</option>
+	<option value="CSC 490">CSC 490</option>
+	<option value="CSC 498">CSC 498</option>
+	<option value="CSC 500">CSC 500</option>
+	<option value="CSC 520">CSC 520</option>
+	<option value="CSC 521">CSC 521</option>
+	<option value="MAT214A">MAT214A</option>
+	<option value="PHS 205">PHS 205</option>
+  </select>
+  <input class=searchField type="text" name="displayValue" id="displayValue" 
+         placeholder="Enter forum subject/tag" onfocus="this.select()">
+  <input name="idValue" id="idValue" type="hidden">
+</div>
 	
 	
-	<div class="dropdown">
+	<div style="top:0;" class="dropdown">
   <IMG style="width:25px;height:25px;float:right;"onclick="myFunction()" class="accountIcon" src="../account.png">
   <div id="myDropdown" class="accountContent">
     <a href="http://weblab.salemstate.edu/~csforum/Forum/createForum.html">Create a Forum</a>
-    <a href="#about">About</a>
-    <a href="#contact">Contact</a>
+    <a href="#about">Send a message</a>
   </div>
 	</div>
 	
@@ -72,6 +112,10 @@ END;
 require_once "../database.php";
 
 if(isset($_SESSION['username']) != 0){
-
+echo $_SESSION['username'];
+}
+else{
+	
+echo "No username";
 }
 ?>
