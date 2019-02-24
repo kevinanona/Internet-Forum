2a70f8348adfde2fb34d3a72af3114f4fabb883e
Kevin Melo <k_elloco@hotmail.com>
2019-02-24 11:38:13 -0500

    Made it so only up to 5 forums are displayed and any fewer than that would only show DIVs for however many there are instead of having empty DIVs

--- displayForums.php
+++ displayForums.php
@@ -5,12 +5,14 @@
 <?php
 require_once "../database.php";
 
-$query = mysqli_query($dbcon , "SELECT creator_username, f_title, f_text, forum_subject_id, tag FROM Forum ORDER BY f_timestamp DESC");
+$sql = "SELECT creator_username, f_title, f_text, forum_subject_id, tag FROM Forum ORDER BY f_timestamp DESC";
+$query = mysqli_query($dbcon , $sql);
 $usernameCol = array();
 $titleCol = array();
 $subjectCol = array();
 $tagCol = array();
 $descriptionCol = array();
+$num_rows = mysqli_num_rows($query);
 
 while($row = mysqli_fetch_array($query)){
 $usernameCol[] = $row['creator_username'];
@@ -21,90 +23,30 @@ $descriptionCol[] = $row['f_text'];
 
 }	//puts each row of the Forum table into a separate index in an array called $row
 
-//while($row1 = mysqli_fetch_array($query)){
-
-
-//}
-//while($row2 = mysqli_fetch_array($query)){
-
-
-//}
-//while($row3 = mysqli_fetch_array($query)){
-
-
-//}
-//while($row4 = mysqli_fetch_array($query)){
-
-
-//}
+if($num_rows > 5){ //sets the limit of the variable to 5 so that if there are 20 forums the website wont display all 20 and only shows up to the 5 most recent posts
+	$num_rows = 5;
+}
 
 //*note* the $row variable is an array with ordered values by index representing f_title then f_text then forum_subject_id then tag
-echo $usernameCol[0];
-echo "<br>";
-echo $titleCol[0];
-echo "<br>";
-echo $subjectCol[0];
-echo "<br>";
-echo $tagCol[0];
-echo "<br>";
-echo $descriptionCol[0];
-echo "<br>";
-echo "everything below is the divs";
 echo "<DIV id=forumsContainer>";
+
+for($i = 0; $i< $num_rows; $i++){
+
 echo	"<DIV class=forums>";
  
 
-echo		"<DIV class=creatorUsername> $usernameCol[0]";
+echo		"<DIV class=creatorUsername> $usernameCol[$i]";
 echo		"</DIV>	";
-echo		"<DIV class=forumTitle> Title: $titleCol[0]";
+echo		"<DIV class=forumTitle> Title: $titleCol[$i]";
 echo		"</DIV>";
-echo		"<DIV class=subjectAndTag> Subject: $subjectCol[0] , Tag: $tagCol[0]";
+echo		"<DIV class=subjectAndTag> Subject: $subjectCol[$i] , Tag: $tagCol[$i]";
 echo		"</DIV>";
-echo		"<DIV class=forumDescription> $descriptionCol[0] ";
+echo		"<DIV class=forumDescription> $descriptionCol[$i] ";
 echo		"</DIV>";
-
-
-//this is the current version --------------------------------------------------------------------------
-echo	"</DIV>";
-
-echo	"<DIV class=forums>";
-echo		"<DIV class=creatorUsername> $usernameCol[1]";
-echo		"</DIV>	";
-echo		"<DIV class=forumTitle> Title: $titleCol[1]";
-echo		"</DIV>";
-echo		"<DIV class=subjectAndTag> Subject: $subjectCol[1] , Tag: $tagCol[1]";
-echo		"</DIV>";
-echo		"<DIV class=forumDescription> $descriptionCol[1] ";
-echo		"</DIV>";
-
 
 echo	"</DIV>";
+}
 
-echo	"<DIV class=forums>";
-echo		"<DIV class=creatorUsername> $usernameCol[2]";
-echo		"</DIV>	";
-echo		"<DIV class=forumTitle> Title: $titleCol[2]";
-echo		"</DIV>";
-echo		"<DIV class=subjectAndTag> Subject: $subjectCol[2] , Tag: $tagCol[2]";
-echo		"</DIV>";
-echo		"<DIV class=forumDescription> $descriptionCol[2] ";
-echo		"</DIV>";
-
-
-echo	"</DIV>";
-
-echo 	"<DIV class=forums>";
-echo		"<DIV class=creatorUsername> $usernameCol[3]";
-echo		"</DIV>	";
-echo		"<DIV class=forumTitle> Title: $titleCol[3]";
-echo		"</DIV>";
-echo		"<DIV class=subjectAndTag> Subject: $subjectCol[3] , Tag: $tagCol[3]";
-echo		"</DIV>";
-echo		"<DIV class=forumDescription> $descriptionCol[3] ";
-echo		"</DIV>";
-
-
-echo	"</DIV>";
 echo "</DIV>";
 
 ?>
