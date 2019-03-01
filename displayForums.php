4b374dd97983ab3c1924519137248a28caba55ae
Kevin Melo <k_elloco@hotmail.com>
2019-02-28 21:56:20 -0500

    Added hover effects for forums in the webpage

--- displayForums.php
+++ displayForums.php
@@ -5,7 +5,7 @@
 <?php
 require_once "../database.php";
 
-$sql = "SELECT creator_username, f_title, f_text, forum_subject_id, tag FROM Forum ORDER BY f_timestamp DESC";
+$sql = "SELECT creator_username, f_title, f_text, s_title, tag FROM Forum, Subject WHERE Forum.forum_subject_id = Subject.s_id ORDER BY f_timestamp DESC";
 $query = mysqli_query($dbcon , $sql);
 $usernameCol = array();
 $titleCol = array();
@@ -17,7 +17,7 @@ $num_rows = mysqli_num_rows($query);
 while($row = mysqli_fetch_array($query)){
 $usernameCol[] = $row['creator_username'];
 $titleCol[] = $row['f_title'];
-$subjectCol[] = $row['forum_subject_id'];
+$subjectCol[] = $row['s_title'];
 $tagCol[] = $row['tag'];
 $descriptionCol[] = $row['f_text'];
 
@@ -27,7 +27,7 @@ if($num_rows > 5){ //sets the limit of the variable to 5 so that if there are 20
 	$num_rows = 5;
 }
 
-//*note* the $row variable is an array with ordered values by index representing f_title then f_text then forum_subject_id then tag
+//*note* the $row variable is an array with ordered values by index representing f_title then f_text then s_title then tag
 echo "<DIV id=forumsContainer>";
 
 for($i = 0; $i< $num_rows; $i++){
@@ -39,8 +39,8 @@ echo		"<DIV class=creatorUsername> $usernameCol[$i]";
 echo		"</DIV>	";
 echo		"<DIV class=forumTitle> Title: $titleCol[$i]";
 echo		"</DIV>";
-echo		"<DIV class=subjectAndTag> Subject: $subjectCol[$i] , Tag: $tagCol[$i]";
-echo		"</DIV>";
+echo		"<DIV class=subjectAndTag><DIV class=testing> $subjectCol[$i] $tagCol[$i]";
+echo		"</DIV></DIV>";
 echo		"<DIV class=forumDescription> $descriptionCol[$i] ";
 echo		"</DIV>";
 
