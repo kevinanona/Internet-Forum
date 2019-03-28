5bece3f56af872dbe464d8582e707fe0f186a20b
Kevin Melo <k_elloco@hotmail.com>
2019-03-28 18:43:46 -0400

    div onclick calls a function not yet created to display comments. 'Recent Forum' text was added into the DIV and removed from Loggedin.php file

--- displayForums.php
+++ displayForums.php
@@ -5,7 +5,8 @@
 <?php
 require_once "../database.php";
 
-$sql = "SELECT creator_username, f_title, f_text, s_title, tag FROM Forum, Subject WHERE Forum.forum_subject_id = Subject.s_id ORDER BY f_timestamp DESC";
+$sql = "
+SELECT creator_username, f_title, f_text, s_title, tag FROM Forum, Subject WHERE Forum.forum_subject_id = Subject.s_id ORDER BY f_timestamp DESC";
 $query = mysqli_query($dbcon , $sql);
 $usernameCol = array();
 $titleCol = array();
@@ -28,12 +29,13 @@ if($num_rows > 7){ //sets the limit of the variable to 5 so that if there are 20
 }
 
 //*note* the $row variable is an array with ordered values by index representing f_title then f_text then s_title then tag
-echo "<DIV id=forumsContainer>";
+echo "<DIV onclick=\"displayComments()\" id=forumsContainer>"; //DIV runs a js function to display comments
 
+echo "<h2>Recent Forums</h2>";
 for($i = 0; $i< $num_rows; $i++){
 
-echo	"<DIV class=forums>";
- 
+echo	"<DIV name=\"$titleCol[$i]\" class=forums>";
+
 
 echo		"<DIV class=creatorUsername> $usernameCol[$i]";
 echo		"</DIV>	";
