3dd367a3deda6e0c1abceb7bbea731839c42070b
Kevin Melo <k_elloco@hotmail.com>
2019-04-03 00:16:17 -0400

    The file now receives through _GET the value that pertains to the clicked forum to be able to display that forum's comments.

--- comment.php
+++ comment.php
@@ -1,6 +1,5 @@
-<!DOCTYPE HTML>
-<HTML><BODY>
 <?php
+include "header.php"; //file has the navigation bar to be included into every logged in page
 /**
  * Created by PhpStorm.
  * User: k_ell
@@ -9,42 +8,56 @@
  */
 
 require_once "../database.php";
-//query
-$sql = '
-SELECT parent_comment_id, comment_forum_id, creator_username, comment_text, c_timestamp
-FROM Comments 
-ORDER BY c_timestamp';
-$query = mysqli_query($dbcon, $sql); //performs query
-$num_rows = mysqli_num_rows($query);
 
-$creatorUsername = array(); //array where all the comment creators will be held
-$commentText = array(); //array where all the comments will be held
-$timestamp = array();
+if(isset($_GET['forumID'])){
+    $f_id = $_GET['forumID']; //gets the value from the submitted form and sets it as f_id variable. Now I know the ID of the forum clicked and can use that info to display the forums comments
+    echo 'We have the variable: ' . $f_id . '<br>';
+    ////query
+    $sql = "
+    SELECT f_title, parent_comment_id, comment_forum_id, Comments.creator_username, comment_text, c_timestamp
+    FROM Comments, Forum
+    WHERE Forum.f_id = Comments.comment_forum_id AND comment_forum_id = $f_id ORDER BY c_timestamp";
 
-while($row = mysqli_fetch_array($query)) { //loop that sets the value of every column to it's array
-    $creatorUsername[] = $row['creator_username'];
-    $commentText[] = $row['comment_text'];
-    $timestamp[] = $row['c_timestamp'];
+    //run query
+    $query = mysqli_query($dbcon, $sql);
+    $parentComment = array();
+    $commentCreator = array();
+    $commentText = array();
+    $commentTitle = array();
+
+//put values in an associative array
+    while($row = mysqli_fetch_array($query)){
+        //puts each column in an array
+        $parentComment[] = $row['parent_comment_id'];
+        $commentCreator[] = $row['creator_username'];
+        $commentText[] = $row['comment_text'];
+        $commentTitle[] = $row['f_title'];
+    }
+    ?>
+
+    <div id="selectedForumInfo">
+        <?php echo "$commentTitle[0]";?>
+    </div>
+    <div id="selectedForumComments">
+
+    </div>
+
+    <?php
+    //TODO query to get number of comments
+    $sql = "";
+
+    $query = "";
+
+    for($i = 0; $i < 1; $i++) {
+
+    }
+
+
+
+
 }
-
-echo "<div class='commentContainer'>";
-for ($i=0;$i<$num_rows; $i++) {
-
-    echo	"<DIV  class='comments'>";
-
-
-    echo		"<DIV class=commentCreator> $creatorUsername[$i]";
-    echo		"</DIV>	";
-    echo		"<DIV class=commentText> $commentText[$i]";
-    echo		"</DIV>";
-    echo		"<DIV class=timestamp> $timestamp[$i] ";
-    echo		"</DIV>";
-
-    echo	"</DIV>";
-
+else {
+    echo 'No variable';
 }
-echo "</div>";
-
-
 ?>
 </BODY></HTML>
