0c58fe0b968a7ffe39a7f4dd5c60c9326e9684fb
Kevin Melo <k_elloco@hotmail.com>
2019-04-05 16:44:20 -0400

    Wrote query to get number of comments in the forum so the code knows how many comment DIVs to create.

--- comment.php
+++ comment.php
@@ -11,19 +11,19 @@ require_once "../database.php";
 
 if(isset($_GET['forumID'])){
     $f_id = $_GET['forumID']; //gets the value from the submitted form and sets it as f_id variable. Now I know the ID of the forum clicked and can use that info to display the forums comments
-    echo 'We have the variable: ' . $f_id . '<br>';
     ////query
-    $sql = "
-    SELECT f_title, parent_comment_id, comment_forum_id, Comments.creator_username, comment_text, c_timestamp
+    $sql = '
+    SELECT f_title, f_text, parent_comment_id, comment_forum_id, Comments.creator_username, comment_text, c_timestamp
     FROM Comments, Forum
-    WHERE Forum.f_id = Comments.comment_forum_id AND comment_forum_id = $f_id ORDER BY c_timestamp";
+    WHERE Forum.f_id = Comments.comment_forum_id AND comment_forum_id = ' . $f_id . ' ORDER BY c_timestamp';
 
     //run query
     $query = mysqli_query($dbcon, $sql);
     $parentComment = array();
     $commentCreator = array();
     $commentText = array();
-    $commentTitle = array();
+    $forumTitle = array();
+    $forumText = array();
 
 //put values in an associative array
     while($row = mysqli_fetch_array($query)){
@@ -31,33 +31,52 @@ if(isset($_GET['forumID'])){
         $parentComment[] = $row['parent_comment_id'];
         $commentCreator[] = $row['creator_username'];
         $commentText[] = $row['comment_text'];
-        $commentTitle[] = $row['f_title'];
+        $forumTitle[] = $row['f_title'];
+        $forumText[] = $row['f_text'];
     }
     ?>
 
     <div id="selectedForumInfo">
-        <?php echo "$commentTitle[0]";?>
+        <div class="commentCreator">
+            <?php echo "$commentCreator[0]";?>
+        </div>
+        <div class="f_title">
+            <?php echo "$forumTitle[0]";?>
+        </div>
+        <div class="f_text">
+            <?php echo "$forumText[0]";?>
+        </div>
     </div>
-    <div id="selectedForumComments">
+<?php
 
-    </div>
+    //query successfully retrieves count based on forum ID
+    $sql = 'SELECT COUNT(c_id) AS "Comment Count"
+FROM Comments
+WHERE comment_forum_id = ' . $f_id . '
+GROUP BY comment_forum_id';
 
-    <?php
-    //TODO query to get number of comments
-    $sql = "";
+    $query = mysqli_query($dbcon, $sql);
 
-    $query = "";
-
+    $commentCount = mysqli_fetch_row($query);
+    echo "comment count is $commentCount[0]";
     for($i = 0; $i < 1; $i++) {
 
     }
 
+?>
+    <div class="commentContainer">
+            <?php
+            for($i = 0; $i < $commentCount; $i++){ //this loop keeps creating DIV blocks for each comment?>
+                <div class="comment">
+                    <!-- TODO - WRITE ALL OF THE COMMENT INFO -->
+                </div>
+            <?php } //this page block just to close the for loop ?>
+    </div>
 
-
-
-}
+<?php
+} // closes the entire if
 else {
-    echo 'No variable';
+    echo 'No variable'; //used for testing to see if the forum ID was sent
 }
 ?>
 </BODY></HTML>
