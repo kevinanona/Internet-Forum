a380300bbda8be4d831df1e9479dc6c6c31a3227
Kevin Melo <k_elloco@hotmail.com>
2019-05-01 17:08:58 -0400

    The user is now presented with a list view of the search results displayed with the same template as the displayForums.php file instead of showing the most recent search result

--- searchForum.php
+++ searchForum.php
@@ -2,101 +2,71 @@
 <?php
 include 'header.php';
 require_once '../database.php';
-echo '*A*';
 if (isset($_GET['subjectTitle'])) {
-    $s_title = $_GET['subjectTitle'];
-    echo $s_title;
-    $sql = '
+        $s_title = $_GET['subjectTitle'];
+        $sql = '
 SELECT f_id, creator_username, f_title, f_text, s_title, tag 
 FROM Forum, Subject 
 WHERE Forum.forum_subject_id = Subject.s_id
 AND s_title = "' . $s_title . '"
 ORDER BY f_timestamp DESC';
-    $query = mysqli_query($dbcon , $sql);
-    $f_id = array();
-    $usernameCol = array();
-    $titleCol = array();
-    $subjectCol = array();
-    $tagCol = array();
-    $descriptionCol = array();
-    $num_rows = mysqli_num_rows($query);
-    echo ' "' .$s_title. '"'; //testing
+        $query = mysqli_query($dbcon, $sql);
+        //put the code inside an if statement to check if the result even exists and if there are no rows then do something else
+        $resultsExist = mysqli_num_rows($query);
 
-    while($row = mysqli_fetch_array($query)){
-        $f_id[] = $row['f_id'];
-        $usernameCol[] = $row['creator_username'];
-        $titleCol[] = '' . $row['f_title'];
-        $subjectCol[] = $row['s_title'];
-        $tagCol[] = $row['tag'];
-        $descriptionCol[] = $row['f_text'];
+        if ($resultsExist != 0) {
+        $f_id = array();
+        $usernameCol = array();
+        $titleCol = array();
+        $subjectCol = array();
+        $tagCol = array();
+        $descriptionCol = array();
+        $num_rows = mysqli_num_rows($query);
+        //echo ' "' .$s_title. '"'; //testing
 
-    }	//puts each row of the Forum table into a separate index in an array called $row
-    ?>
-<div id="selectedForumInfo">
-    <div class="f_creator">
-        <?php echo "Creator: $usernameCol[0]";?>
-    </div>
-    <div class="f_title">
-        <?php echo "$titleCol[0]";?>
-    </div>
-    <div class="f_text">
-        <?php echo "$descriptionCol[0]";?>
-    </div>
-</div>
-    <!-- div for creating comments -->
-    <div class="newComment">
-        <form METHOD="POST" ACTION="comment.php?forumID=<?php echo "$f_id"; //this ACTION resends the forum ID parameter to the page so as to still display the forum being looked at?>">
-            <textarea placeholder="Contribute to the discussion.." id="comment" name="comment" required
-                <?php if(empty($_SESSION['username'])){echo "onclick=\"noComment()\"";}?>></textarea>
-            <input type="submit" id="submitComment">
-        </form>
-    </div>
-    <?php
-    //inserts the entered comment
-    if(isset($_POST['comment'])){
-        if(isset($_SESSION['username'])){
-            $newComment = $_POST['comment'];
-            $username = $_SESSION['username'];
-            $sql = 'INSERT INTO Comments (comment_forum_id, creator_username, comment_text) VALUES (\'' . $f_id . '\',\'' . $username . '\',\'' . $newComment . '\')';
-            $query = mysqli_query($dbcon, $sql);
-        }
-        else {
-            echo "<script>noComment()</script>";
-        }
+        while ($row = mysqli_fetch_array($query)) {
+            $f_id[] = $row['f_id'];
+            $usernameCol[] = $row['creator_username'];
+            $titleCol[] = '' . $row['f_title'];
+            $subjectCol[] = $row['s_title'];
+            $tagCol[] = $row['tag'];
+            $descriptionCol[] = $row['f_text'];
+
+        }    //puts each row of the Forum table into a separate index in an array called $row
+            if($num_rows > 7){ //sets the limit of the variable to 7 so that if there are more than 7 forums the website wont display all of them and only shows up to the 7 most recent posts
+                $num_rows = 7;
+            }
+
+//*note* the $row variable is an array with ordered values by index representing f_title then f_text then s_title then tag
+            echo "<DIV id=forumsContainer>";
+            echo "<h2>Recent Forums</h2>";
+            for($i = 0; $i< $num_rows; $i++){
+                $nameOfForum = $titleCol[$i];
+                echo	"<DIV id=\"$titleCol[$i]\" onclick=\"displayComment('' . $nameOfForum)\" class=forums>";
+//echo	"<DIV id=\"$titleCol[$i]\" class=forums>";
+
+
+                echo		"<DIV class=creatorUsername> $usernameCol[$i]</DIV>	";
+                echo		"<DIV class=forumTitle> Title: $titleCol[$i]</DIV>";
+                echo		"<DIV class=subjectAndTag><DIV class=innerSubjectAndTag> $subjectCol[$i] $tagCol[$i]</DIV></DIV>";
+                echo		"<DIV class=forumDescription> $descriptionCol[$i]"; ?>
+                <FORM METHOD="GET" ACTION="comment.php">
+                    <input type="submit" value=<?php echo "\"$f_id[$i]\""; ?> name="forumID">
+                </FORM>
+
+                <?php
+                echo "</DIV>"; //this DIV closes the forumDescription DIV
+                echo	"</DIV>";
+
+//print_r($usernameCol); testing
+            }
+
+            echo "</DIV>"; //closes forumsContainer
     }
-    ?>
-
-    <?php
-
-    //query successfully retrieves count based on forum ID
-    $sql = 'SELECT COUNT(c_id) AS "Comment Count"
-FROM Comments
-WHERE comment_forum_id = ' . $f_id . '
-GROUP BY comment_forum_id';
-
-    $query = mysqli_query($dbcon, $sql);
-
-    $commentCount = mysqli_fetch_row($query);
-    //echo "comment count is $commentCount[0]"; //used for testing
-
-    ?>
-    <div class="commentContainer">
-        <?php
-        for($i = 0; $i < $commentCount[0]; $i++){ //this loop keeps creating DIV blocks for each comment?>
-            <div class="comment">
-                <div class="c_creator">
-                    <!-- Comment Creator -->
-                    <?php echo "Comment creator: $commentCreator[$i]"; ?>
-                </div>
-                <div class="c_text">
-                    <!-- Comment Text -->
-                    <?php echo "$commentText[$i]"; ?>
-                </div>
-            </div>
-        <?php } //this page block just to close the for loop ?>
-    </div>
-<?php
+    else {
+        echo "There are Currently no forums with this subject";
     }
+}
 else {
     echo "No subject selected";
 }
